f49f70b7d8b67b0cd942e213ad33a5dd2b87f893
Alger <AlgerDu@outlook.com>
2021-03-28 20:33:42 +0800

    add attributes

---
+++ Src/Infrastructures/CustomerCli/Attributes/CmdAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Infrastructures.CustomerCli
+{
+    /// <summary>
+    /// 命令的描述
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class CmdAttribute : Attribute
+    {
+        /// <summary>
+        /// 命令码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 命令的帮助描述
+        /// </summary>
+        public string Descriptions { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="code"></param>
+        public CmdAttribute(string code)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ Src/Infrastructures/CustomerCli/Attributes/SwitchMapKeyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Infrastructures.CustomerCli
+{
+    /// <summary>
+    /// 命令可选参数的 mapkey
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SwitchMapKeyAttribute : Attribute
+    {
+        /// <summary>
+        /// key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        public SwitchMapKeyAttribute(string key)
+        {
+            Key = key;
+        }
+    }
+}
