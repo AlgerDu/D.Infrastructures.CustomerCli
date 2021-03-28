0478214f106f4d556fa470fb2960a6eb37a43ded
Alger <AlgerDu@outlook.com>
2021-03-28 20:42:12 +0800

    example 项目

---
+++ Example/D.Example.TestCli/Cmds/ShowWordCmd.cs
@@ -0,0 +1,12 @@
+﻿using D.Infrastructures.CustomerCli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Example.TestCli
+{
+    [Cmd("show-word", Descriptions = "test for output word")]
+    public class ShowWordCmd
+    {
+    }
+}
