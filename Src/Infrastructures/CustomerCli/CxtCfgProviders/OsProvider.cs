429e08fa5d062119ef0f14930bdd052ad43fd6fe
Alger <AlgerDu@outlook.com>
2021-03-29 22:38:20 +0800

    Create OsProvider.cs

---
+++ Src/Infrastructures/CustomerCli/CxtCfgProviders/OsProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace D.Infrastructures.CustomerCli
+{
+    /// <summary>
+    /// 收集操作系统的信息
+    /// </summary>
+    public class OsProvider : ICmdContextConfigProvider
+    {
+        ///<inheritdoc/>
+        public IConfiguration Get()
+        {
+            var os = string.Empty;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                os = "win";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                os = "linux";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                os = "osx";
+            }
+            else
+            {
+                os = "unknown";
+            }
+
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "os", os }
+                })
+                .Build();
+        }
+    }
+}
