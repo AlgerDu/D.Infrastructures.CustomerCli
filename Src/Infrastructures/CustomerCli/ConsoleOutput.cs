1c0a789d3fdf6c44ced6b196f15259ce1be42b13
Alger <AlgerDu@outlook.com>
2021-04-03 08:10:50 +0800

    Update ConsoleOutput.cs

--- Src/Infrastructures/CustomerCli/ConsoleOutput.cs
+++ Src/Infrastructures/CustomerCli/ConsoleOutput.cs
@@ -4,11 +4,18 @@ using System.Text;
 
 namespace D.Infrastructures.CustomerCli
 {
+    /// <summary>
+    /// 一个线程安全的控制台输出
+    /// </summary>
     public class ConsoleOutput : IConsoleOutput
     {
+        /// <inheritdoc/>
         public void Write(string format, params object[] args)
         {
-            throw new NotImplementedException();
+            lock (this)
+            {
+                Console.WriteLine(format, args);
+            }
         }
     }
 }
