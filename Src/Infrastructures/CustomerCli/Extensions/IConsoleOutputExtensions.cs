e5ff6f476ba1131124062ca33e81712ea6f81018
Alger <AlgerDu@outlook.com>
2021-04-05 15:17:09 +0800

    Create IConsoleOutputExtensions.cs

---
+++ Src/Infrastructures/CustomerCli/Extensions/IConsoleOutputExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Infrastructures.CustomerCli
+{
+    public static class IConsoleOutputExtensions
+    {
+        public static void BeginScope(this IConsoleOutput output)
+        {
+            output.ScopeLevel++;
+        }
+
+        public static void EndScope(this IConsoleOutput output)
+        {
+            output.ScopeLevel--;
+        }
+    }
+}
