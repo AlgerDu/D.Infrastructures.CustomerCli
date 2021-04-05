f7314771f2235374db016e768ba8a4d6a2273955
Alger <AlgerDu@outlook.com>
2021-04-05 15:27:17 +0800

    Update IConsoleOutputExtensions.cs

--- Src/Infrastructures/CustomerCli/Extensions/IConsoleOutputExtensions.cs
+++ Src/Infrastructures/CustomerCli/Extensions/IConsoleOutputExtensions.cs
@@ -6,14 +6,25 @@ namespace D.Infrastructures.CustomerCli
 {
     public static class IConsoleOutputExtensions
     {
-        public static void BeginScope(this IConsoleOutput output)
+        public static IConsoleOutput BeginScope(this IConsoleOutput output)
         {
             output.ScopeLevel++;
+
+            return output;
         }
 
-        public static void EndScope(this IConsoleOutput output)
+        public static IConsoleOutput EndScope(this IConsoleOutput output)
         {
             output.ScopeLevel--;
+
+            return output;
+        }
+
+        public static IConsoleOutput NewLine(this IConsoleOutput output)
+        {
+            output.WriteLine("");
+
+            return output;
         }
     }
 }
