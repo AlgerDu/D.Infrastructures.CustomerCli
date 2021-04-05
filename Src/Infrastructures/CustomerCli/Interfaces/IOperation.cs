7e2be44cf321f63e5d4697b5fd4c9fc69a5f7ec8
Alger <AlgerDu@outlook.com>
2021-04-05 22:29:59 +0800

    Update IOperation.cs

--- Src/Infrastructures/CustomerCli/Interfaces/IOperation.cs
+++ Src/Infrastructures/CustomerCli/Interfaces/IOperation.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace D.Infrastructures.CustomerCli
 {
     /// <summary>
-    /// 一个命令内部划分为一些小的操作
+    /// 一个命令内部划分为一些小的操作；
+    /// 不同条件下处理的逻辑不同
     /// </summary>
     public interface IOperation
     {
