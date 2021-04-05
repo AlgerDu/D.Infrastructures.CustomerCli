8d1e2abbdd0480b71720d9cf11d987b8f224638a
Alger <AlgerDu@outlook.com>
2021-04-05 16:01:18 +0800

    Create EnvironmentProvider.cs

---
+++ Src/Infrastructures/CustomerCli/CxtCfgProviders/EnvironmentProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace D.Infrastructures.CustomerCli
+{
+    /// <summary>
+    /// 提供一些基础的环境参数
+    /// </summary>
+    public class EnvironmentProvider : ICmdContextConfigProvider
+    {
+
+        /// <inheritdoc/>
+        public IConfiguration Get()
+        {
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "workDir", Directory.GetCurrentDirectory() }
+                })
+                .Build();
+        }
+    }
+}
