09ad371c8a51721294d48581d2ddb2d358014172
Alger <AlgerDu@outlook.com>
2021-04-05 16:17:57 +0800

    Update DefaultCliApp.cs

--- Src/Infrastructures/CustomerCli/DefaultCliApp.cs
+++ Src/Infrastructures/CustomerCli/DefaultCliApp.cs
@@ -14,6 +14,7 @@ namespace D.Infrastructures.CustomerCli
         readonly ICmdContext _context;
         readonly ICmdProvider _supportCmds;
         readonly IServiceProvider _services;
+        readonly IConsoleOutput _output;
 
         /// <summary>
         /// 默认构造函数
@@ -22,22 +23,27 @@ namespace D.Infrastructures.CustomerCli
         /// <param name="context"></param>
         /// <param name="supportCmds"></param>
         /// <param name="services"></param>
+        /// <param name="output"></param>
         public DefaultCliApp(
             ILogger<DefaultCliApp> logger
             , ICmdContext context
             , ICmdProvider supportCmds
             , IServiceProvider services
+            , IConsoleOutput output
             )
         {
             _logger = logger;
             _context = context;
             _supportCmds = supportCmds;
             _services = services;
+            _output = output;
         }
 
         /// <inheritdoc/>
         public void Execute()
         {
+            OutputAppDescription();
+
             var code = _context.CmdCode();
 
             var isSupport = _supportCmds.Supports.TryGetValue(code, out var cmdType);
@@ -48,6 +54,18 @@ namespace D.Infrastructures.CustomerCli
 
                 cmd.Execute();
             }
+        }
+
+        private void OutputAppDescription()
+        {
+            var tool = _context.GetAppDescription();
+
+            _output.WriteLine($"{tool.Name}    {tool.Version}");
+            _output.NewLine();
+
+            _output.WriteLine($"location = [{tool.Location}]");
+            _output.WriteLine($"workDir = [{_context.GetWorkDir()}]");
+            _output.NewLine();
         }
     }
 }
