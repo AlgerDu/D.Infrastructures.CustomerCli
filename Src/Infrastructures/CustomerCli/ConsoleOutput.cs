7e2a2a4905700b271917d5178506e951966167db
Alger <AlgerDu@outlook.com>
2021-04-05 15:17:12 +0800

    Update ConsoleOutput.cs

--- Src/Infrastructures/CustomerCli/ConsoleOutput.cs
+++ Src/Infrastructures/CustomerCli/ConsoleOutput.cs
@@ -9,13 +9,47 @@ namespace D.Infrastructures.CustomerCli
     /// </summary>
     public class ConsoleOutput : IConsoleOutput
     {
+        private int _scopeLevel = 0;
+        private string _space;
+
+        /// <inheritdoc/>
+        public int ScopeLevel
+        {
+            get => _scopeLevel;
+            set
+            {
+                lock (this)
+                {
+                    _scopeLevel = value < 0 ? 0 : value;
+
+                    _space = new StringBuilder()
+                        .Append(' ', _scopeLevel * 4)
+                        .ToString();
+                }
+            }
+        }
+
         /// <inheritdoc/>
         public void Write(string format, params object[] args)
         {
             lock (this)
             {
-                Console.Write(format, args);
+                Console.Write(PreHandle(format), args);
             }
+        }
+
+        /// <inheritdoc/>
+        public void WriteLine(string format, params object[] args)
+        {
+            lock (this)
+            {
+                Console.WriteLine(PreHandle(format), args);
+            }
+        }
+
+        private string PreHandle(string format)
+        {
+            return _space + format;
         }
     }
 }
