9f4faaf0873348a85b68161fc46bf22ee3cfa707
Alger <AlgerDu@outlook.com>
2021-04-05 08:45:56 +0800

    Update IConsoleOutput.cs

--- Src/Infrastructures/CustomerCli/Interfaces/IConsoleOutput.cs
+++ Src/Infrastructures/CustomerCli/Interfaces/IConsoleOutput.cs
@@ -10,10 +10,22 @@ namespace D.Infrastructures.CustomerCli
     public interface IConsoleOutput
     {
         /// <summary>
+        /// 作用域层级（前面的空格层级）
+        /// </summary>
+        int ScopeLevel { get; set; }
+
+        /// <summary>
         /// 格式化输出
         /// </summary>
         /// <param name="format"></param>
         /// <param name="args"></param>
         void Write(String format, params object[] args);
+
+        /// <summary>
+        /// 格式化输出
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        void WriteLine(String format, params object[] args);
     }
 }
