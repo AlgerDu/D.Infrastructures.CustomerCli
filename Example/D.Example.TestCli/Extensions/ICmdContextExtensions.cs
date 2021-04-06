f45fe6aeb7a6a4dcc97e1c6ce98f37155575c729
Alger <AlgerDu@outlook.com>
2021-04-06 21:40:01 +0800

    Create ICmdContextExtensions.cs

---
+++ Example/D.Example.TestCli/Extensions/ICmdContextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using D.Infrastructures.CustomerCli;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace D.Example.TestCli
+{
+    public static class ICmdContextExtensions
+    {
+        public static void AddYml(this ICmdContext context, string file)
+        {
+            var config = new ConfigurationBuilder()
+                .AddYamlFile(Path.Combine(context.GetWorkDir(), file))
+                .Build();
+
+            context.AddConfig(config);
+        }
+    }
+}
